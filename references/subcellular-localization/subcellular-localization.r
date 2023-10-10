256d620e274f2041b5a79d17c6a1a9613115beea
H. Emre Etlioglu <etlioglu@protonmail.com>
2023-10-10 08:47:44 +0000

    subcellular localization

---
+++ references/subcellular-localization/subcellular-localization.r
@@ -0,0 +1,12 @@
+# bioMart seems to still have not-at-all-informative attribute names for the cellular localization GO terms
+# https://support.bioconductor.org/p/39425/#39426
+
+# Integrated subcellular localization dataset from http://comppi.linkgroup.hu/downloads
+library(tidyverse)
+
+int_sub_cell_loc <- read.delim("comppi--proteins_locs--tax_hsapiens_loc_all.txt") |>
+  mutate(ensembl_id = str_extract(Synonyms, "ENSG\\d*")) |>
+  dplyr::select(c("ensembl_id", "Major.Loc.With.Loc.Score", "Minor.Loc")) |>
+  distinct()
+
+write.csv(int_sub_cell_loc, "int_sub_cell_loc.csv")
