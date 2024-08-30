57d0a99a5dcb1d607abf0d3e51d7cd3f701c31f8
H. Emre Etlioglu <emre.etlioglu@sckcen.be>
2024-08-30 14:53:45 +0200

    various scripts

---
+++ scripts/primer_tm.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+
+# Helps designing primers of given length and Tm value, for example for RACE (Rapid Amplification of cDNA ends) reactions which might require primers with unusually high Tm values for specificity purposes.
+# Scans a nucleotide sequence and reports oligonucleotides of interest (with given length and Tm value).
+# Uses BioPerl and oligotm of Primer3.
+
+# Takes a nucleotide sequence, a oligonucleotide size and a Tm value and reports the oligonucleotides with the given(or higher) Tm values. Employs oligotm of Primer3 and BioPerl.
+# Useful when designing primers with specific Tm values.
+
+use strict;
+use warnings;
+use Bio::SeqIO;
+
+# get the name of the fasta file
+my $fastafile = shift;
+
+print "primer length:\t";
+my $primerlength = <>;
+
+print "target Tm:\t";
+my $targettm = <>;
+
+my $input = Bio::SeqIO->new(
+                            -file   => $fastafile,
+                            -format => 'Fasta',
+                            );
+                                                    
+                                                       
+while (my $query = $input->next_seq) {
+	my $processcount = 0;
+	my $querytitle = $query->id;
+	my $querylength = $query->length;
+	my $subseqstart = 0;
+	my $subseqend = 0;
+		while ($processcount <= $querylength-$primerlength) {
+			$processcount++;
+			my $subseqstart = $processcount; 
+			my $subseqend = $subseqstart+$primerlength-1;
+			my $seqstr = $query->subseq($subseqstart,$subseqend);
+			my $tm=`oligotm -tp 1 -sc 1 $seqstr`;
+				if ($tm >= $targettm) {
+					print "$querytitle\t$subseqstart\t$subseqend\t$seqstr\t$tm\n";
+	 	}
+	 }	
+}
